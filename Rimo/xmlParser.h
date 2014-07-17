f5e71e10a1cf22f31dce46575de1da8b8684f0d2
sladjannimcevic <macnovi@Sladjans-Mac.local>
2014-07-17 10:13:17 +0200

    pocetak
    Pocetna podesavanja za projekat

---
+++ Rimo/xmlParser.h
@@ -0,0 +1,13 @@
+//
+//  xmlParser.h
+//  testgDATAxml
+//
+//  Created by Sladjan on 7/15/14.
+//  Copyright (c) 2014 Sladjan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface xmlParser : NSObject
+-(void)parse;
+@end
