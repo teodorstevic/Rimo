5a0d249dfbbb0d025671fe533b9649228322d085
Mac novi <macnovi@Sladjans-Mac.local>
2014-07-17 09:33:15 +0200

    Initial Commit

---
+++ RimoTests/RimoTests.m
@@ -0,0 +1,34 @@
+//
+//  RimoTests.m
+//  RimoTests
+//
+//  Created by Mac novi on 7/17/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RimoTests : XCTestCase
+
+@end
+
+@implementation RimoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
